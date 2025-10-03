b53855b0e95b13a6e31c7485cfc64ba9144fdcbb
RonkLonk <riley.irwin.nz@gmail.com>
2025-10-03 13:39:57 +1300

    Buoyancy Implementation
    Everything is jank I swear I will refactor soon ;-;
    
    - Vertical move speed is now based on 'buoyancy' which increases/decreases the longer the up/down input is held
    - This is designed to replicate a Buoyancy Control Device (BCD) in real life, and may consume oxygen from the scuba tank in future.

--- Scripts/diving_player.gd
+++ Scripts/diving_player.gd
@@ -1,36 +1,45 @@
 extends CharacterBody2D
 
 @export var speed = 100
+@export var buoyancy_acceleration = 1
 @export var spawn_x = 350
 @export var spawn_y	= 350
 @onready var camera = $Camera2D
 @onready var _animated_sprite = $DivingPlayerAnimation
 @onready var _can_move
+@onready var buoyancy
+@onready var max_buoyancy
+
 
 func _ready():
 	# Set the player's position on spawn
 	self.position = Vector2(spawn_x, spawn_y) 
 	self._can_move = true
-	#self.hide()
+	self.buoyancy = 0 #neutrally buoyant
+	self.max_buoyancy = 50
 	
 func _process(_delta):
 	# flip horizontally by inverting scale.x when the cursor is on the other half of the screen
 	var cursor_angle = rad_to_deg(get_local_mouse_position().angle())
-	print(cursor_angle)
 	if (cursor_angle < -90) or (cursor_angle > 90):
 		self.scale.x = self.scale.x * -1
 
-	# play fisherman_walk animation if any directional input detected
+	# play player_swim animation constantly
 	_animated_sprite.play("player_swim")
 
 func _physics_process(delta):
 	get_input()
 	move_and_slide()
 	
-	#move player based on diectional input
 func get_input():
 	if _can_move:
 		var input_direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-		velocity = input_direction * speed
+		if Input.is_action_pressed("ui_up") or Input.is_action_pressed("ui_down"):
+			if abs(buoyancy) <= max_buoyancy:
+				buoyancy += input_direction.y * buoyancy_acceleration
+			else:
+				buoyancy = max_buoyancy * input_direction.y
+		velocity.x = input_direction.x * speed
+		velocity.y = buoyancy
 	else:
-		velocity = Vector2(0, 0)
+		velocity = Vector2(0, buoyancy)
